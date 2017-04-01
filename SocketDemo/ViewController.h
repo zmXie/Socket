e99e7cee28e38f46ed22ad595c15f777848c427b
CHT-Technology <cht@CHT-TechnologydeiMac.local>
2017-04-01 09:15:24 +0800

    Initial Commit

---
+++ SocketDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SocketDemo
+//
+//  Created by CHT-Technology on 2017/4/1.
+//  Copyright © 2017年 CHT-Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SocketDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SocketDemo
+//
+//  Created by CHT-Technology on 2017/4/1.
+//  Copyright © 2017年 CHT-Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
